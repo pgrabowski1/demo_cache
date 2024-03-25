8a2de93b88cf191b5bbd37160d3258e4a0e15059
Piotr <pgbercik@gmail.com>
2024-03-25 23:43:35 +0100

    link to tutorial

--- src/main/java/com/example/demo_cache/DemoCacheApplication.java
+++ src/main/java/com/example/demo_cache/DemoCacheApplication.java
@@ -8,6 +8,7 @@ public class DemoCacheApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoCacheApplication.class, args);
+        /*https://www.baeldung.com/spring-boot-ehcache*/
     }
 
 }
